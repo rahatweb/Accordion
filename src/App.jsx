10c2e46563f6cf244dae3b3be63b020f3e4f5584
rahatadnan <rahatadnan786@gmail.com>
2024-02-04 02:16:30 +0600

    only accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Accordion from "./Components/accordion";
-import QRCodeGenerator from "./Components/Qrcode";
 const App = () => {
   return (
     <div>
       <Accordion />
-      <QRCodeGenerator />
     </div>
   );
 };
